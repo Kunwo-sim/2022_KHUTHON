f9bbb8f72225fb5000e8d1828ff41ffed6392698
SKW <rjsdntlsdj@gmail.com>
2022-09-30 19:17:43 +0900

    [Add] 씬 연동

--- Assets/Scripts/EasterEgg.cs
+++ Assets/Scripts/EasterEgg.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EasterEgg : MonoBehaviour
 {
     public void ChagneScene()
     {
+        string name = SceneManager.GetActiveScene().name;
 
+        if (name == "Mobile")
+        {
+            SceneManager.LoadScene("InGame");
+        }
+        else if (name == "InGame")
+        {
+            SceneManager.LoadScene("Mobile");
+        }
     }
 
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 }
