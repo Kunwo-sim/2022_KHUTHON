c56f908b653898afd5151aa0aff66dcf207db3ca
Kim Hyeonwoo <1reds@naver.com>
2022-11-15 06:04:54 +0900

    Add Intro Video

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,12 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-
     bool isMulti = false;
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private GameObject displayVideoTexture;
+
+    [SerializeField]
+    private Button startButton;
+    
+    // private void Start()
+    // {
+    //     videoPlayer.loopPointReached += HandleOnVideoFinished;
+    // }
+
+    // private void HandleOnVideoFinished(VideoPlayer vp)
+    // {
+    //     displayVideoTexture.SetActive(false);
+    // }
+
+    private void Update()
+    {
+        if (videoPlayer.frame >= 90)
+        {
+            startButton.gameObject.SetActive(true);
+        }
+    }
+
     public void GameStartBtnClicked()
     {
         SceneManager.LoadScene("InGame");
